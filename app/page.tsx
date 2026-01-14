7b280ad9db01246886efcd610136979caf2ab185
ginwan <ginwan697@gmail.com>
2026-01-14 17:45:07 +0400

    project setup done

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Welcome to the nextjs16!</div>
+    <section>
+      <h1 className='text-center'>The Hub For Every Dev <br /> Event You can&lsquo;t Miss</h1>
+      <p className='text-center mt-5'>Hackathons, Meetups, and Conferences all in one place.</p>
+    </section>
   )
 }
 
